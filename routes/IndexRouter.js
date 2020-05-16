45df4cbef9399e7ef968ca791eda57c903d6e530
e3dos <euzakmac@hotmail.com>
2020-05-16 01:36:40 -0300

    novo diagrama

--- routes/IndexRouter.js
+++ routes/IndexRouter.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+/* About page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
 module.exports = router;
